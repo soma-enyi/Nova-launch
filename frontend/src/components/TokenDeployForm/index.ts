79629617c9f38dbc309cf7faa79c768fad997375
JOASH <joashgideon02@gmail.com>
2026-02-20 17:46:24 +0100

    feat:Implemented MetadataStep Component And Implemented ReviewStep Component

--- frontend/src/components/TokenDeployForm/index.ts
+++ frontend/src/components/TokenDeployForm/index.ts
@@ -1,4 +1,7 @@
 export { BasicInfoStep } from './BasicInfoStep';
 export { LogoUploadStep } from './LogoUploadStep';
+export { MetadataStep } from './MetadataStep';
+export { ReviewStep } from './ReviewStep';
 export type { BasicInfoData } from './BasicInfoStep';
+export type { MetadataData } from './MetadataStep';
 
